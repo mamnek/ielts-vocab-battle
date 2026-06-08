657cf5671f0a2889439f7df7acacff0346a241f6
mamnek <lekimchau49@gmail.com>
2026-06-09 00:41:55 +0700

    Update app.py

--- ielts_arena/app.py
+++ ielts_arena/app.py
@@ -306,4 +306,4 @@ def on_save_vocab_set(data):
 if __name__ == '__main__':
     # Sẵn sàng triển khai Render: Lấy cổng động từ biến môi trường
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
